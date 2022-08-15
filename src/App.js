6ccf0b0eed8ab5b1d84fbd35294376994a61e276
jprodriguez2020 <jprodriguez2020@gmail.com>
2022-08-15 17:55:22 -0300

    cambios en el footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import Main from './layouts/Main.jsx'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+import { Fragment } from 'react';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1 className="text-3xl font-bold underline">Home</h1>} />
-      <Route path="/products" element={<h1>Products</h1>} />
-    </Routes>
+    <Fragment>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/products" element={<h1>Products</h1>} />
+      </Routes>
+      <Footer />
+    </Fragment>
   );
 }
 
