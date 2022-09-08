86129f5c2cd826e0134dbf17c8ae2b9ec1dc29a6
danielkraus14 <danielk14@servimatsrl.com.ar>
2022-09-07 22:47:19 -0300

    changes in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,102 +1,104 @@
-import React, {useReducer, useContext, createContext} from 'react'
-
-export const PRODUCTS = [
-    {
-        id: 1,
-        name: 'Product 1',
-        price: 10,
-        image: 'https://via.placeholder.com/150'
-    },
-    {
-        id: 2,
-        name: 'Product 2',
-        price: 20,
-        image: 'https://via.placeholder.com/155'
-    },
-    {
-        id: 3,
-        name: 'Product 3',
-        price: 30,
-        image: 'https://via.placeholder.com/160'
-    },
-    {
-        id: 4,
-        name: 'Product 4',
-        price: 40,
-        image: 'https://via.placeholder.com/165'
-    },
-    {
-        id: 5,
-        name: 'Product 5',  
-        price: 50,
-        image: 'https://via.placeholder.com/170'
-    },
-    {
-        id: 6,
-        name: 'Product 6',
-        price: 60,
-        image: 'https://via.placeholder.com/175'
-    }
-];
-
+import React, { useReducer, useContext, createContext } from "react";
 
 const CartStateContext = createContext();
 const CartDispatchContext = createContext();
 
 const cartReducer = (state, action) => {
-    switch (action.type) {
-        case 'ADD_TO_CART':
-            return {
-                ...state,
-                cartItems: [...state.cartItems, action.payload],
-                cartTotal: state.cartTotal + action.payload.price,
-                totalItems: state.totalItems + 1
-            }
-        case 'REMOVE_FROM_CART':
-            return {
-                ...state,
-                cartItems: state.cartItems.filter(item => item.id !== action.payload),
-                cartTotal: state.cartTotal - action.payload.price,
-                totalItems: state.totalItems - 1
-            }
-        case 'UPDATE_CART_ITEM':
-            return {
-                ...state,
-                cartItems: state.cartItems.map(item => {
-                    if (item.id === action.payload.id) {
-                        return action.payload
-                    } else {
-                        return item
-                    }
-                })
-            }
-        case 'CLEAR_CART':
-            return {
-                ...state,
-                cartItems: []
-            }
-        default:
-            return state
-    }
+  switch (action.type) {
+    case "ADD_ITEM_TO_CART":
+    console.log(action.payload)
+      let itemInCart = state.cartItems.find((item) => item.id === action.payload.id);
+      return itemInCart
+        ? {
+            ...state,
+            cartItems: state.cartItems.map((item) =>
+              item.id === action.payload.id
+                ? { ...item, quantity: item.quantity + 1 }
+                : item
+            ),
+            cartTotal: state.cartTotal + action.payload.attributes.price,
+            totalItems: state.totalItems + 1,
+          }
+        : {
+            ...state,
+            cartItems: [...state.cartItems, { ...action.payload, quantity: 1 }],
+            cartTotal: state.cartTotal + action.payload.attributes.price,
+            totalItems: state.totalItems + 1,
+          };
+    case "REMOVE_ITEM_FROM_CART":
+      let itemToDeleteInCart = state.cartItems.find(
+        (item) => item.id === action.payload.id
+      );
+      console.log(itemToDeleteInCart);
+      if(itemToDeleteInCart.quantity !== undefined){
+        return itemToDeleteInCart && itemToDeleteInCart.quantity > 1
+        ? {
+            ...state,
+            cartItems: state.cartItems.map((item) =>
+              item.id === action.payload.id
+                ? { ...item, quantity: item.quantity - 1 }
+                : item
+            ),
+            cartTotal: state.cartTotal - action.payload.attributes.price,
+            totalItems: state.totalItems - 1,
+          }
+        : {
+            ...state,
+            cartItems: state.cartItems.filter(
+              (item) => item.id !== action.payload.id
+            ),
+            cartTotal: state.cartTotal - action.payload.attributes.price,
+            totalItems: state.totalItems - 1,
+          };}
+          return {...state}
+    case "REMOVE_FROM_CART":
+      let itemToRemoveInCart = state.cartItems.find(
+        (item) => item.id === action.payload.id
+      );
+      let quantityItemToRemove = itemToRemoveInCart.quantity;
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (item) => item.id !== action.payload.id
+        ),
+        cartTotal:
+          state.cartTotal - action.payload.attributes.price * quantityItemToRemove,
+        totalItems: state.totalItems - quantityItemToRemove,
+      };
+    case "UPDATE_CART_ITEM":
+      return {
+        ...state,
+        cartItems: state.cartItems.map((item) => {
+          if (item.id === action.payload) {
+            return action.payload;
+          } else {
+            return item;
+          }
+        }),
+      };
+    case "CLEAR_CART":
+      return initialState;
+    default:
+      return state;
+  }
 };
 
 const initialState = {
-    cartItems: [],
-    totalItems: 0,
-    cartTotal: 0
+  cartItems: [],
+  totalItems: 0,
+  cartTotal: 0,
 };
 
-export const CartProvider = ({children}) => {
-    const [cart, dispatch] = useReducer(cartReducer, initialState);
-    return (
-        <CartStateContext.Provider value={cart}>
-        <CartDispatchContext.Provider value={dispatch}>
-            {children}
-        </CartDispatchContext.Provider>
-        </CartStateContext.Provider>
-    );
-    }
-
+export const CartProvider = ({ children }) => {
+  const [cart, dispatch] = useReducer(cartReducer, initialState);
+  return (
+    <CartStateContext.Provider value={cart}>
+      <CartDispatchContext.Provider value={dispatch}>
+        {children}
+      </CartDispatchContext.Provider>
+    </CartStateContext.Provider>
+  );
+};
 
 export const useCart = () => useContext(CartStateContext);
 export const useDispatchCart = () => useContext(CartDispatchContext);
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,30 +1,63 @@
 import React from "react";
 import bg_dog from "../assets/img/bg-dog.png";
-import { PRODUCTS } from "../components/Cart/Cart";
 import { useDispatchCart } from "../components/Cart/Cart";
+import axios from 'axios';
+import { useState } from "react";
+import { useEffect } from "react";
+
 
 const Main = () => {
 
+  const [products, setProducts] = useState([])
+  
+  useEffect(  () => { 
+  
+    async function fetchProducts(){
+
+      const response = await axios.get('http://localhost:1337/api/products?populate=image')
+      const data = response.data.data
+      setProducts(data)
+      console.log(data)
+    }
+    fetchProducts()
+  }, [])
+
+  
+
+  /* axios.get('http://localhost:1337/api/certificates?populate=english,spanish,portuguese')
+    .then( res => console.log(res.data.data))
+    .catch( err => console.log(err)) */
   const dispatch = useDispatchCart();
   const handleAddToCart = (product) => {
-    dispatch({type: 'ADD_TO_CART', payload: product})
+    dispatch({type: 'ADD_ITEM_TO_CART', payload: product})
+  }
+
+  const handleRemoveToCart = (product) => {
+    dispatch({type: 'REMOVE_ITEM_FROM_CART', payload: product})
   }
   return (
     <div className="container-fluid">
-      <div className="flex bg-themeYellow h-50 justify-end w-screen">
+      <div className="flex justify-end w-screen bg-themeYellow h-50">
         <div className="mt-20">
-          {PRODUCTS.map((product) => (
-            <div className="flex justify-between items-center p-3" key={product.id}>
+          {products.map((product) => (
+            	
+            <div className="flex items-center justify-between p-3" key={product.id}>
               <div className="flex items-center">
                 <button 
-                  className="bg-themeYellow text-themeMainBrown text-md font-bold py-2 px-4 rounded-lg"
+                  className="px-4 py-2 font-bold rounded-lg bg-themeYellow text-themeMainBrown text-md"
                   onClick={() => handleAddToCart(product)}
                 >
                   Add to cart
                 </button>
-                <img src={product.image} alt={product.name} className="w-8 h-8 mr-2" />
-                <div className="text-sm">{product.name}</div>
-                <div className="text-sm">{product.price}</div>
+                {/* <img src={`http://localhost:1337${product.attributes.image.data[0].attributes.formats.large.url}`} alt={product.attributes.name} className="w-8 h-8 mr-2" />
+ */}                <div className="text-sm">{product.attributes.name}</div>
+                <div className="text-sm">{product.attributes.price}</div>
+                <button 
+                  className="px-4 py-2 font-bold rounded-lg bg-themeYellow text-themeMainBrown text-md"
+                  onClick={() => handleRemoveToCart(product)}
+                >
+                  Remove from cart
+                </button>
               </div>
             </div>
           ))}
