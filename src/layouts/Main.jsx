968638e7d2cc6a6fd9ca12bfccb7a9eb7d95af4a
Gonzalo Moreno <gonn.m@hotmail.com>
2022-09-08 16:45:18 -0300

    Correcciones varias

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 import bg_dog from '../assets/img/bg-dog.png';
 import Profitable from '../components/Profitable';
 import Brands from '../components/Brands/Brands';
-import Footer from '../components/Footer/Footer';
 
 const Main = () => {
   return (
     <div className="container-fluid">
-      <div className="flex justify-end w-screen bg-themeYellow h-50">
+      <div className="flex justify-end bg-themeYellow h-50">
         <img src={bg_dog} alt="background dog" className="max-w-lg mt-20" />
       </div>
       <Profitable />
       <Brands />
-      <Footer />
     </div>
   );
 };
