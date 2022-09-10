39edcf0c662869ad87397b7a0e64a96b5f32cf5b
jprodriguez2020 <jprodriguez2020@gmail.com>
2022-09-10 13:05:46 -0300

    Mejora en Responsive Footer

--- src/components/Footer/ItemsContainer.jsx
+++ src/components/Footer/ItemsContainer.jsx
@@ -3,7 +3,7 @@ import { ABOUTUS, PRODUCTS, PAYMENTANDSHIPPING, LEGAL } from "./Menus";
 
 const ItemsContainer = () => {
   return (
-    <div className="grid grid-cols-1 gap-12 px-12 py-8 sm:grid-cols-3 lg:grid-cols-4 sm:px-8 text-themeYellow">
+    <div className="grid grid-cols-1 gap-12 px-12 py-8 sm:grid-cols-1 lg:grid-cols-4 sm:px-8 text-themeYellow">
       <Item Links={ ABOUTUS } title="ABOUT US" />
       <Item Links={ PRODUCTS } title="PRODUCTS" />
       <Item Links={ PAYMENTANDSHIPPING } title="PAYMENT AND SHIPPING" />
